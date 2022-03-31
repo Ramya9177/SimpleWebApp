23dad403e249fd27461e92f87b3f324814e6bcfd
Ramya9177 <97871929+Ramya9177@users.noreply.github.com>
2022-03-31 16:39:50 +0530

    Update HelloRestController.java

--- src/main/java/com/zensar/HelloRestController.java
+++ src/main/java/com/zensar/HelloRestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloRestController {
   
-  @GetMapping("/api/message")
+  @GetMapping("/hello")
   public String sayHello(){
     return "Welcome to zensar";
   }
